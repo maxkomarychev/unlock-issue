6e38bd5af4ba9b6a4eecfb3e2791914ea3243406
Max Komarychev <maxkomarychev@gmail.com>
2019-10-16 09:36:07 +0300

    Deploy v0.2.1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# unlock-issue
+# Unlock issue
+
+Unlock issue
+
+This action is a wrapper for one of [octokit's](https://octokit.github.io/rest.js) methods.
+
+Original docs can be found here: https://octokit.github.io/rest.js/#octokit-routes-issues-unlock
+
+# Usage
+
+```yaml
+- uses: maxkomarychev/unlock-issue@v0.2.1
+  with:
+    token: ${{ secrets.GITHUB_TOKEN }}
+    issue_number: 100
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+function parse_array(input_name) {
+  const input_value = core.getInput(input_name)
+  if (input_value === "") {
+    return undefined; 
+  }
+  if (input_value === "<<EMPTY>>") {
+    return [];
+  }
+  return input_value.split(",");
+}
+
+function parse_boolean(input_name) {
+  const input_value = core.getInput(input_name)
+  if (!input_name) {
+    return false
+  }
+  return input_name === "true"
+}
+
+function default_parse(input_name) {
+  const input_value = core.getInput(input_name)
+  return input_value || undefined
+}
+
+try {
+  const token = default_parse("token");
+  const issue_number = default_parse("issue_number");
+  const client = new github.GitHub(token);
+  const context = github.context;
+  client.issues.unlock({
+    ...context.repo,
+      token,
+      issue_number,
+    headers: {
+      "Accept": "application/vnd.github.flash-preview+json, application/vnd.github.ant-man-preview+json",
+    }
+  }).then(response => {
+    console.log('response', response)
+  })
+  .catch(error => {
+    console.log("error 1", error);
+    core.setFailed(error.message);
+  });
+} catch (error) {
+  console.log("error 2", error);
+  core.setFailed(error.message);
+}
